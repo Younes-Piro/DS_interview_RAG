0e70eb921c7695ea49840b5fb193a48e9637f38d
Younes-Piro <piroyounes148@gmail.com>
2023-10-06 15:21:58 +0100

    mergin all dfs

---
+++ transform/transformation.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import os
+
+folder_path = './assets'
+
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+
+dataframes = []  # List to store DataFrames for each CSV file
+
+for csv_file in csv_files:
+    file_path = os.path.join(folder_path, csv_file)
+    df = pd.read_csv(file_path)
+    dataframes.append(df)
+
+# Assuming you have a list of DataFrames called 'dataframes'
+merged_df = pd.concat(dataframes, ignore_index=True)
+
+print(merged_df.head())
